1bf6bd44ccb87058da5ccab34775e87fecfa69f8
anusesachin <jut.amazon.acc@gmail.com>
2024-07-05 20:18:03 +0530

    first commit

---
+++ src/common/Addition.java
@@ -0,0 +1,19 @@
+package common;
+
+/** This class is designed for addition purpose  */
+public class Addition {
+
+	/** This is zero param constructor */
+	public Addition() {
+	}
+
+	/** This method adds two integers */
+	public int add( int a, int b ) {
+		return a + b;
+	}
+
+	/** This method contains main logic */
+	public static void main( String[] args ) {
+		//main logic
+	}
+}
---
+++ src/q2/Demo.java
@@ -0,0 +1,12 @@
+package q2;
+
+import common.Employee;
+
+public class Demo {
+	public static void main( String[] args ) {
+		//here create object of Employee class and add 101,sam,1000 data into that
+		Employee e = new Employee();
+
+		//display that data here
+	}
+}
